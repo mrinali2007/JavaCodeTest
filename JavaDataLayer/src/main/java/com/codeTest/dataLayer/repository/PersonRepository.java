614822afb1a5e5900c9a6df1a0888efb0d8c0f37
Mrinali Agarwal <mrinali.agarwal@M309633L9HTD6.community.veritas.com>
2019-11-11 11:08:41 +0000

    maven project updates

--- JavaDataLayer/src/main/java/com/codeTest/dataLayer/repository/PersonRepository.java
+++
@@ -1,8 +0,0 @@
-package com.codeTest.dataLayer.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.codeTest.dataLayer.entity.Person;
-
-public interface PersonRepository extends MongoRepository<Person, Integer> {
-
-}
